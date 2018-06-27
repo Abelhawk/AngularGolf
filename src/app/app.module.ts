38926bb0571023b8a31026749a5e52f85366430a
Austin Ballard <abelhawk@gmail.com>
2018-06-26 21:51:04 -0600

    Slowly but surely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
-import { ColumnsComponent } from './columns/columns.component';
+import { ColumnsComponent } from './components/columns/columns.component';
+import { SelectTeeComponent } from './components/select-tee/select-tee.component';
+import { MatButtonModule, MatRadioModule } from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColumnsComponent
+    ColumnsComponent,
+    SelectTeeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatRadioModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tee.ts
@@ -0,0 +1,4 @@
+export class Tee {
+  name: string;
+  yardage: Array<number>;
+}
